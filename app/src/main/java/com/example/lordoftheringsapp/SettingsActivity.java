8f17362b0b7a5d5889ed3ade88b078a71b140d0f
fgojkovic <38782443+fgojkovic@users.noreply.github.com>
2021-07-03 21:30:05 +0200

    Added list prefrences for fonts

--- app/src/main/java/com/example/lordoftheringsapp/SettingsActivity.java
+++ app/src/main/java/com/example/lordoftheringsapp/SettingsActivity.java
@@ -1,15 +1,28 @@
 package com.example.lordoftheringsapp;
 
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.FontRequest;
+import android.provider.FontsContract;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.ListPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
+import java.util.ArrayList;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    private CharSequence[] fonts;
+    private Context context;
+
+    private ListPreference fontPreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +38,31 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        context = getBaseContext();
+
+        Handler handler = new Handler();
+
+
+        fontPreference = new ListPreference(getApplicationContext());
+        FontRequest request = new FontRequest("com.example.fontprovider",
+                "com.example.fontprovider", "my font");
+
+        FontsContract.FontRequestCallback callback =
+                new FontsContract.FontRequestCallback() {
+                    @Override
+                    public void onTypefaceRetrieved(Typeface typeface) {
+                        // Your code to use the font goes here
+                        String typefaceString = typeface.toString();
+                    }
+
+                    @Override
+                    public void onTypefaceRequestFailed(int reason) {
+                        // Your code to deal with the failure goes here
+                        Toast.makeText(getApplicationContext(), "Typeface failed", Toast.LENGTH_SHORT).show();
+                    }
+                };
+        FontsContract.requestFonts(context, request, handler, null, callback);
 
     }
 
